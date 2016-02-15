52adfee735d00d9507d53dc5fe402ec2bef6c75d
Farzad Ghanei <farzad.ghanei@gmail.com>
2016-02-15 16:23:49 +0800

    Add a file to run tests with code coverage
    If python version is 3.2 skip coverage since coverage does not support
    python 3.2

---
+++ run-test-coverage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import platform
+import subprocess
+import sys
+import os
+
+python_version = platform.python_version_tuple()[:2]
+is_py32 = python_version == (3, 2)
+no_coverage = os.getenv('NO_COVERAGE', '')
+
+if __name__ == '__main__':
+    if is_py32:
+        print("coverage.py does not support Python 3.2.", file=sys.stderr)
+        print("Running tests without coverage ...", file=sys.stderr)
+        subprocess.call("python setup.py test", shell=True)
+    elif no_coverage != '':
+        print("Running tests without coverage ...", file=sys.stderr)
+        subprocess.call("python setup.py test", shell=True)
+    else:
+        subprocess.call("coverage run setup.py test", shell=True)
+
+
