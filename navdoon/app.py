8b372ef1f4e09bcefd849bcc5b44c14cc22ddaf7
Farzad Ghanei <farzad.ghanei@gmail.com>
2016-02-21 14:58:50 +0800

    App configures logger, and creates the destinations specs for server
    The app configures the server (its logger, destinations, etc) then
    runs the server.
    Now when the server runs it starts collectors and makes sure that enough
    destinations are available to meet the requested configurations.
    This is so that the server can be used to reload in a graceful way,
         recreating the destinations, and recreate collectors (if required),
    without loosing the queue.

--- navdoon/app.py
+++ navdoon/app.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import logging.handlers
 from argparse import ArgumentParser, FileType
 from threading import Lock
 from signal import signal, SIGINT, SIGTERM, SIGHUP
@@ -9,24 +10,40 @@ except ImportError:
     import configparser
 import navdoon
 from navdoon.server import Server
-from navdoon.destination import Stream, Graphite
+from navdoon.destination import Stdout, Graphite
 
 
 log_level_names = ('DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL', 'CRITICAL')
+
+
+def parse_config_file(file_):
+    parser = configparser.SafeConfigParser()
+    parser.readfp(file_)
+    config = dict()
+    for key, value in parser.items('navdoon'):
+        if key == 'config':
+            continue
+        elif key == 'log-level' and value not in log_level_names:
+            raise ValueError("Invalid log level '{}' in configuration file '{}'".format(
+                value, file_.name))
+        config[key.replace('-', '_')] = value
+    return config
 
 
 class App(object):
     def __init__(self, args):
         self._config = dict()
         self._args = args
-        self._configure(args)
         self._server = None
+        self._logger = None
         self._run_lock = Lock()
         self._shutdown_lock = Lock()
         self._reload_lock = Lock()
+        self._configure(args)
 
     def __del__(self):
         self.shutdown()
+        self._close_logger()
 
     @staticmethod
     def get_description():
@@ -52,37 +69,27 @@ class App(object):
     def get_config(self):
         return self._config
 
-    def create_logger(self):
-        logger = logging.Logger('navdoon')
-        return logger
+    def get_logger(self):
+        if not self._logger:
+            self._logger = self._create_logger()
+        return self._logger
 
-    def create_collectors(self):
-        collectors = []
-        args = self._args
-        if args.get('flush-stdout'):
-            collectors.append(Stream(sys.stdout))
-
-        if args.get('flush-stderr'):
-            collectors.append(Stream(sys.stderr))
-
-        if args.get('flush-graphite'):
-            collectors.append(Graphite())
-
-        return collectors
-
-
-    def create_destinations(self):
-        raise NotImplemented()
+    def get_destinations(self):
+        destinations = []
+        if self._config.get('flush_stdout'):
+            destinations.append((Stdout, ()))
+        if self._config.get('flush_graphite'):
+            for graphite_address in self._config['flush_graphite'].split(','):
+                graphite_address = graphite_address.strip().split(':')
+                graphite_host = graphite_address.pop(0).strip()
+                graphite_port = graphite_address and int(graphite_address.pop()) or 2003
+                destinations.append((Graphite, (graphite_host, graphite_port)))
+        return destinations
 
     def create_server(self):
         server = Server()
-        server.logger = self.create_logger()
-        destinations = self.create_destinations()
-        collectors = self.create_destinations()
-        for dest in destinations:
-            server.add_destination(dest)
-        for collector in collectors:
-            server.add_collector(collector)
+        server.logger = self.get_logger()
+        server.set_destinations(self.get_destinations())
         return server
 
     def run(self):
@@ -90,46 +97,6 @@ class App(object):
             self._register_signal_handlers()
             self._server = self.create_server()
             self._server.start()
-
-    def _configure(self, args):
-        parsed_args = vars(self._parse_args(args))
-        configs = self.get_default_config()
-        if parsed_args['config'] is not None:
-            with parsed_args['config'] as config_file:
-                configs = self._parse_config_file(config_file)
-
-        for key, value in parsed_args.items():
-            if value is not None:
-                configs[key] = value
-
-        self._config = configs
-
-    def _parse_config_file(self, file_):
-        parser = configparser.SafeConfigParser()
-        parser.readfp(file_)
-        config = dict()
-        for key, value in parser.items('navdoon'):
-            if key == 'config':
-                continue
-            elif key == 'log-level' and value not in log_level_names:
-                raise ValueErorr("Invalid log level '{}' in configuration file '{}'".format(
-                    value, file_.name))
-            config[key.replace('-', '_')] = value
-        return config
-
-    def _parse_args(self, args):
-        parser = ArgumentParser(description=self.get_description())
-        parser.add_argument('-c', '--config', help='path to config file', type=FileType('r'))
-        parser.add_argument('--log-level', help='logging level', choices=log_level_names)
-        parser.add_argument('-l', '--log', help='path to log file')
-        parser.add_argument('--log-stderr', help='log to stderr')
-        parser.add_argument('--syslog', action='store_true', help='log to syslog')
-        return parser.parse_args(args)
-
-    def _register_signal_handlers(self):
-        signal(SIGINT, self._handle_signal_int)
-        signal(SIGTERM, self._handle_signal_term)
-        signal(SIGHUP, self._handle_signal_hup)
 
     def shutdown(self):
         with self._shutdown_lock:
@@ -142,7 +109,42 @@ class App(object):
             if not self._server:
                 raise Exception("App is not running, can not reload")
             self._configure(self.args)
+            logger = self._create_logger()
+            destinations = self.get_destinations()
+            self._close_logger()
+            self._logger = logger
+            self._server.logger = logger
+            self._server.set_destinations(destinations)
             self._server.reload()
+
+    def _configure(self, args):
+        parsed_args = vars(self._parse_args(args))
+        configs = self.get_default_config()
+        if parsed_args['config'] is not None:
+            with parsed_args['config'] as config_file:
+                configs = parse_config_file(config_file)
+
+        for key, value in parsed_args.items():
+            if value is not None:
+                configs[key] = value
+
+        self._config = configs
+
+    def _parse_args(self, args):
+        parser = ArgumentParser(description=self.get_description())
+        parser.add_argument('-c', '--config', help='path to config file', type=FileType('r'))
+        parser.add_argument('--log-level', help='logging level', choices=log_level_names)
+        parser.add_argument('--log-file', help='path to log file')
+        parser.add_argument('--log-stderr', help='log to stderr')
+        parser.add_argument('--log-syslog', action='store_true', help='log to syslog')
+        parser.add_argument('--flush-stdout', help='flush to standard output')
+        parser.add_argument('--flush-graphite', help='flush to graphite', default=None)
+        return parser.parse_args(args)
+
+    def _register_signal_handlers(self):
+        signal(SIGINT, self._handle_signal_int)
+        signal(SIGTERM, self._handle_signal_term)
+        signal(SIGHUP, self._handle_signal_hup)
 
     def _handle_signal_int(self):
         self.shutdown()
@@ -152,6 +154,28 @@ class App(object):
 
     def _handle_signal_hup(self):
         self.reload()
+
+    def _create_logger(self):
+        log_level_name = self._config.get('log_level', 'INFO')
+        if log_level_name not in log_level_names:
+            raise ValueError("invalid log level " + log_level_name)
+        logger = logging.Logger('navdoon')
+        logger.setLevel(getattr(logging, log_level_name))
+        if self._config.get('log_stderr'):
+            logger.addHandler(logging.StreamHandler(sys.stderr))
+        if self._config.get('log_file'):
+            logger.addHandler(logging.FileHandler(self._config['log_file']))
+        if self._config.get('log_syslog'):
+            logger.addHandler(logging.handlers.SysLogHandler())
+        return logger
+
+    def _close_logger(self):
+        if self._logger:
+            for handler in self._logger.handlers:
+                if hasattr(handler, 'close') and callable(handler.close):
+                    handler.close()
+                self._logger.removeHandler(handler)
+        self._logger = None
 
 
 def main(args):
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,24 @@
+import sys
+import logging
+import logging.handlers
 import unittest
 from os import path
-from navdoon.app import App
+from navdoon.app import App, parse_config_file
+from navdoon.destination import Stdout, Graphite
+
+test_config_file_path = path.join(path.join(path.dirname(__file__), 'fixtures'), 'test_config.ini')
+
+
+class TestParseConfigFile(unittest.TestCase):
+    def test_parse_config_file(self):
+        with open(test_config_file_path) as test_config_file:
+            config = parse_config_file(test_config_file)
+        self.assertEqual('WARN', config['log_level'])
 
 
 class TestApp(unittest.TestCase):
     def setUp(self):
-        self.config_filename = path.join(path.join(path.dirname(__file__), 'fixtures'), 'test_config.ini')
+        self.config_filename = test_config_file_path
 
     def test_get_args(self):
         app = App(())
@@ -18,14 +31,45 @@ class TestApp(unittest.TestCase):
     def test_get_config(self):
         app = App(())
         conf = app.get_config()
-        self.assertFalse(conf['syslog'])
+        self.assertFalse(conf['log_syslog'])
 
-        app = App(('--syslog', '-c', self.config_filename))
+        app = App(('--log-syslog', '-c', self.config_filename))
         conf = app.get_config()
-        self.assertTrue(conf['syslog'])
+        self.assertTrue(conf['log_syslog'])
         self.assertEqual('WARN', conf['log_level'])
 
     def test_args_overwrite_config(self):
         app = App(('--log-level', 'ERROR', '-c', self.config_filename))
         conf = app.get_config()
         self.assertEqual('ERROR', conf['log_level'])
+
+    def test_get_logger(self):
+        app = App(('--config', self.config_filename, '--log-syslog'))
+        logger = app.get_logger()
+        self.assertEqual(logging.WARN, logger.getEffectiveLevel())
+        self.assertEqual(2, len(logger.handlers))
+        stderr_handler, syslog_handler = logger.handlers
+        self.assertIsInstance(stderr_handler, logging.StreamHandler)
+        self.assertEqual(stderr_handler.stream, sys.stderr)
+        self.assertIsInstance(syslog_handler, logging.handlers.SysLogHandler)
+
+    def test_get_destinations(self):
+        app = App(['--config', self.config_filename])
+        destinations = app.get_destinations()
+        self.assertEqual(1, len(destinations))
+        self.assertEqual([(Stdout, ())], destinations)
+
+        app_flush_graphite = App(['--flush-graphite', 'localhost'])
+        destinations = app_flush_graphite.get_destinations()
+        self.assertEqual(1, len(destinations))
+        self.assertEqual([(Graphite, ('localhost', 2003))], destinations)
+
+        app_multi_flush = App(['--config', self.config_filename, '--flush-graphite', 'example.org:2006,localhost'])
+        destinations = app_multi_flush.get_destinations()
+        self.assertEqual(3, len(destinations))
+        expected = [
+            (Stdout, ()),
+            (Graphite, ('example.org', 2006)),
+            (Graphite, ('localhost', 2003))
+        ]
+        self.assertEqual(expected, destinations)
