ef5819ab2c38965f46c451b87ae160957a1e4ccb
Farzad Ghanei <farzad.ghanei@gmail.com>
2016-02-29 12:17:59 +0800

    Add docstrings (and fix a string format error) in destinations

--- navdoon/destination/graphite.py
+++ navdoon/destination/graphite.py
@@ -1,9 +1,17 @@
-from navdoon.utils import TCPClient
+"""
+navdoon.destination.graphite
+----------------------------
+A destination to flush metrics to Graphite
+"""
+
 from time import time
+from navdoon.utils import TCPClient
 from navdoon.destination.abstract import AbstractDestination
 
 
 class Graphite(TCPClient, AbstractDestination):
+    """Flush metrics to Graphtie over a TCP connection"""
+
     def __init__(self, host='localhost', port=2003):
         super(Graphite, self).__init__(host, port)
 
@@ -12,6 +20,7 @@ class Graphite(TCPClient, AbstractDestination):
 
     @staticmethod
     def create_request_from_metrics(metrics):
+        """Creates Graphite protocol lines from metrics"""
         requests = []
         for metric in metrics:
             name, value = metric[:2]
@@ -20,14 +29,15 @@ class Graphite(TCPClient, AbstractDestination):
         return requests
 
     def flush(self, metrics):
+        """Flush metrics to Graphite"""
         lines = self.create_request_from_metrics(metrics)
         self._send_lines(lines)
 
     def _send_lines(self, lines):
         num_lines = len(lines)
         data = "\n".join([line.strip() for line in lines]).encode()
-        self._log_debug("flushing %d metrics to graphite on {}:{} ...".format(
+        self._log_debug("flushing {} metrics to graphite on {}:{} ...".format(
             num_lines, self.host, self.port))
         self._send_with_lock(data)
-        self._log("flushed %d metrics to graphite on {}:{}".format(
+        self._log("flushed {} metrics to graphite on {}:{}".format(
             num_lines, self.host, self.port))
--- navdoon/destination/stream.py
+++ navdoon/destination/stream.py
@@ -1,15 +1,24 @@
+"""
+navdoon.destination.stream
+--------------------------
+Define destinations to flush metrics to streams
+"""
+
 import sys
 from time import time
 from navdoon.destination.abstract import AbstractDestination
 
 
 class Stream(AbstractDestination):
+    """Destination to flush metrics to stream"""
+
     def __init__(self, file_handle):
         self._file = file_handle
         self.pattern = "{name} {value} {timestamp}"
         self.append = "\n"
 
     def create_output_from_metrics(self, metrics):
+        """Creates the output to be flushed, from the metrics"""
         requests = []
         for metric in metrics:
             name, value = metric[:2]
@@ -20,6 +29,7 @@ class Stream(AbstractDestination):
         return requests
 
     def flush(self, metrics):
+        """Flush metrics to the stream"""
         lines = self.create_output_from_metrics(metrics)
         self._write_lines(lines)
 
@@ -32,5 +42,7 @@ class Stream(AbstractDestination):
 
 
 class Stdout(Stream):
+    """Destination to flush metrics to standard output"""
+
     def __init__(self):
         super(Stdout, self).__init__(sys.stdout)
