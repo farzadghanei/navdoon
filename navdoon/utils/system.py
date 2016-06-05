e939fd2274563af94b7d1160f6a6a6ef1d71df81
Farzad Ghanei <farzad.ghanei@gmail.com>
2016-06-05 21:34:29 +0200

    Add a TaskThreadPool class to system utils (on going)
    To help run some tasks concurrently easier by prestarting some worker
    threads. This is going to be used in TCP server so we can pre start some
    threads and handle TCP requests in them.
    
    The task is not done yet, the ThreadPool will also have to spawn new
    threads if queue is not empty and we need more tests.

--- navdoon/utils/system.py
+++ navdoon/utils/system.py
@@ -6,6 +6,8 @@ System utilities and mixin classes
 
 import platform
 from multiprocessing import cpu_count
+from threading import Thread, RLock, Event
+from navdoon.pystdlib.queue import Queue, Empty
 
 
 PLATFORM_NAME = platform.system().strip().lower()
@@ -26,3 +28,96 @@ def os_syslog_socket():
         freebsd="/var/run/log"
     )
     return syslog_addresses.get(PLATFORM_NAME, None)
+
+
+class TaskThreadPool(object):
+    def __init__(self, size):
+        self._size = int(size)
+        self._threads = []
+        self._queue = Queue()
+        self._task_counter = 0
+        self._task_counter_lock = RLock()
+        self._task_results = dict()
+        self._stop_event = Event()
+
+    def __del__(self):
+        self.stop()
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def threads(self):
+        return self._threads
+
+    def initialize(self):
+        self._queue = Queue()
+        self._refill_worker_threads()
+        self._start_worker_threads()
+        return self
+
+    def do(self, func, *args, **kwargs):
+        if self._stop_event.is_set():
+            raise Exception("Task thread pool has stopped")
+        task_id = self._get_next_task_counter()
+        self._queue.put((task_id, func, args, kwargs))
+        return task_id
+
+    def wait_until_done(self):
+        self._queue.join()
+        return self
+
+    def stop(self, wait=True):
+        self._stop_event.set()
+        if wait:
+            for thread in self._threads:
+                thread.join()
+
+    def get_result(self, task_id):
+        if not task_id in self._task_results:
+            raise ValueError(
+                "no such task exists with id '{}'".format(task_id))
+        return self._task_results[task_id]
+
+    def _get_next_task_counter(self):
+        self._task_counter_lock.acquire()
+        counter = self._task_counter
+        self._task_counter += 1
+        self._task_counter_lock.release()
+        return counter
+
+    def _consume_task_queue(self):
+        while not self._stop_event.is_set():
+            try:
+                self._run_task_from_queue(1)
+            except Empty:
+                pass
+
+    def _run_task_from_queue(self, timeout=None):
+        (task_id, func, args, kwargs) = self._queue.get(True, timeout)
+        result = func(*args, **kwargs)
+        self._queue.task_done()
+        self._task_results[task_id] = result
+
+    def _refill_worker_threads(self):
+        self._threads.extend(
+            self._create_worker_threads(
+                self._size - len(self._threads)
+            )
+        )
+
+    def _start_worker_threads(self):
+        for thread in self._threads:
+            thread.start()
+        self._stop_event.clear()
+        return self
+
+    def _create_worker_threads(self, size):
+        threads = []
+        for i in range(size):
+            threads.append(
+                Thread(target=self._consume_task_queue)
+            )
+
+        return threads
--- tests/test_utils_system.py
+++ tests/test_utils_system.py
@@ -1,5 +1,6 @@
 import unittest
 import navdoon.utils.system
+from navdoon.utils.system import TaskThreadPool
 
 
 def mock_cpu_count(count):
@@ -22,3 +23,59 @@ class TestFunctions(unittest.TestCase):
         navdoon.utils.system.cpu_count = not_implemented
         self.assertEqual(1, navdoon.utils.system.available_cpus())
 
+
+class TestTaskThreadPool(unittest.TestCase):
+    def test_init(self):
+        pool = TaskThreadPool(4)
+        self.assertEquals(pool.size, 4)
+
+    def test_initialize(self):
+        pool = TaskThreadPool(4)
+        pool.initialize()
+        self.assertEqual(len(pool.threads), 4)
+
+    def test_do_tasks(self):
+        pool = TaskThreadPool(4)
+        pool.initialize()
+        executed = []
+
+        def some_task():
+            return executed.append(True)
+
+        for i in range(10):
+            pool.do(some_task)
+
+        pool.wait_until_done()
+        pool.stop()
+        self.assertEqual(executed, [True] * 10)
+
+    def test_check_task_results(self):
+        pool = TaskThreadPool(5)
+        pool.initialize()
+
+        def change_text(text, number):
+            return "{}. {}".format(number, text.upper())
+
+        words = ["this", "will be", "uppercased"]
+        expected_results = ["1. THIS", "2. WILL BE", "3. UPPERCASED"]
+        counter = 1
+        task_ids = []
+
+        for word in words:
+            task_ids.append(pool.do(change_text, word, counter))
+            counter += 1
+
+        pool.wait_until_done()
+        pool.stop()
+        results = []
+
+        for id in task_ids:
+            results.append(pool.get_result(id))
+
+        self.assertEqual(results, expected_results)
+
+    def test_check_task_results_fails_on_invalid_task_id(self):
+        pool = TaskThreadPool(2)
+        pool.initialize()
+        pool.stop()
+        self.assertRaises(ValueError, pool.get_result, 1003)
