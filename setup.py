21d305104cde832a8965aae7637b337b34d2bf11
Farzad Ghanei <farzad.ghanei@gmail.com>
2016-11-22 20:15:06 +0100

    Add dependency to setuptools in setup.py and use typing in there
    setuptools is already available on latest versions of Python, does not
    make sense to fallback on distutils.

--- setup.py
+++ setup.py
@@ -6,22 +6,18 @@ navdoon
 from __future__ import print_function
 
 import os
+from setuptools import setup, find_packages
 
 try:
-    import setuptools
-    from setuptools import setup
+    import distutilazy.test, distutilazy.clean  # type: ignore
 except ImportError:
-    setuptools = None
-    from distutils.core import setup
+    distutilazy = None  # type: ignore
 
-try:
-    import distutilazy.test
-    import distutilazy.clean
-except ImportError:
-    distutilazy = None
 
 from navdoon import (__title__, __summary__, __version__, __author__,
                      __license__)
+
+__all__ = ['setup_params', 'classifiers', 'long_description']  # type: ignore
 
 classifiers = [
     "Development Status :: 4 - Beta", "Intended Audience :: Developers",
@@ -36,14 +32,14 @@ classifiers = [
     "Programming Language :: Python :: Implementation :: CPython",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: System :: Networking :: Monitoring"
-]
+]  # type: ignore
 
-long_description = __summary__
+long_description = __summary__  # type: str
 with open(os.path.join(os.path.dirname(__file__), "README.rst")) as fh:
     long_description = fh.read()
 
 setup_params = dict(name=__title__,
-                    packages=["navdoon", "navdoon.pystdlib", "navdoon.destination", "navdoon.utils"],
+                    packages=find_packages(),
                     version=__version__,
                     description=__summary__,
                     long_description=long_description,
@@ -51,21 +47,18 @@ setup_params = dict(name=__title__,
                     url="https://github.com/farzadghanei/navdoon",
                     license=__license__,
                     classifiers=classifiers,
-                    install_requires=["statsdmetrics>=0.3"])
-
-if setuptools:
-    setup_params["keywords"] = "statsd monitoring"
-    setup_params["test_suite"] = "tests"
-    setup_params["zip_safe"] = False
-    setup_params["extras_require"] = dict(
-        dev=["distutilazy>=0.4.2", "typing>=3.5.0.1", "coverage"]
-    )
-    setup_params["entry_points"] = dict(
-        console_scripts=["navdoon=navdoon.app:main",]
-    )
-else:
-    setup_params["scripts"] = ['bin/navdoon']
-
+                    install_requires=["statsdmetrics>=0.3"],
+                    keywords="statsd monitoring",
+                    test_suite="tests",
+                    zip_safe=False,
+                    extras_require=dict(
+                        dev=["distutilazy>=0.4.2", "typing>=3.5.0.1", "coverage"]
+                    ),
+                    entry_points=dict(
+                        console_scripts=["navdoon=navdoon.app:main", ]
+                    ),
+                    scripts=['bin/navdoon']
+                    )  # type: ignore
 
 if distutilazy:
     setup_params["cmdclass"] = dict(test=distutilazy.test.run_tests,
@@ -74,5 +67,3 @@ if distutilazy:
 
 if __name__ == "__main__":
     setup(**setup_params)
-
-__all__ = ('setup_params', 'classifiers', 'long_description')
